4966d0262995048fab83c79bbce69715064a9be4
Rhamon Silva Almeida <rhamonalmeida.dev@hotmail.com>
2025-11-05 03:07:01 -0300

    corrigido compose

--- src/app/services/site-cadastro.service.ts
+++ src/app/services/site-cadastro.service.ts
@@ -8,7 +8,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class SiteCadastroService {  
-  private apiUrl = `${environment.apiUrl}/empresa`;
+  private apiUrl = `${environment.apiUrl}/empresa/`;
 
   constructor(private http: HttpClient) {
     
